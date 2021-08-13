ac423833ee1d4e48825b5ba92b0e53ec3c4d229e
Mathias Nikkel <mathiasnikkel@gmail.com>
2021-08-13 17:52:15 -0300

    adding jest

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import Button from ".";
+
+describe("Test Component", () => {
+
+  it("Should render correctly", () => {
+    const { container } = render(<Button>Dummy</Button>)
+
+    expect(container).toBeInTheDocument()
+  });
+});
