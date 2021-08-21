aa6be91c021100c485ea14590493fd8cc8025482
Mathias Nikkel <mathias.nikkel@ambevtech.com.br>
2021-08-21 10:52:41 -0300

    refactoring test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Button from '.';
 
 describe('Test Component', () => {
   it('Should render correctly', () => {
-    const { container } = render(<Button>Dummy</Button>);
+    render(<Button>Dummy</Button>);
 
-    expect(container).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /dummy/iu })).toBeInTheDocument();
   });
 });
